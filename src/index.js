d2532b31ef8cf6d0430890d786db3dec99827fed
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-09-24 22:42:39 +0530

    Read effects from DOM (click) events

--- src/index.js
+++ src/index.js
@@ -2,31 +2,47 @@ import Rx from 'rxjs'
 
 const
   // Logic (functional)
-  main = () => ({
-    'DOM': Rx.Observable.timer(0, 1000).map(i => `Seconds elapsed ${i}`),
-    'Log': Rx.Observable.timer(0, 2000).map(i => i * 2)
-  }),
+  main = domSource => {
+    const
+      click$ = domSource
+
+    return {
+      'DOM': click$
+        .startWith(null)
+        .switchMap(() => Rx.Observable.timer(0, 1000).map(i => `Seconds elapsed ${i}`)),
+      'Log': Rx.Observable.timer(0, 2000).map(i => i * 2)
+    }
+  },
 
 
 // Effects (imperative)
   domDriver = text$ => {
+    const
+      domSource = Rx.Observable.fromEvent(document, 'click')
+
     text$.subscribe(text => {
       const
         container = document.querySelector('#app')
 
       container.textContent = text
     })
+
+    return domSource
   },
 
   consoleLogDriver = msg$ => msg$.subscribe(msg => console.log(msg)),
 
-  run = (mainFn, effects) => {
+  run = (mainFn, drivers) => {
     const
-      sink = mainFn()
+      proxyDOMSource = new Rx.Subject(),
+      sink = mainFn(proxyDOMSource),
+      domSource = drivers.DOM(sink.DOM)
 
-    Object.keys(effects).forEach(key => {
-      effects[key](sink[key])
-    })
+    domSource.subscribe(click => proxyDOMSource.next(click))
+
+    // Object.keys(drivers).forEach(key => {
+    //   drivers[key](sink[key])
+    // })
   },
 
   drivers = {
