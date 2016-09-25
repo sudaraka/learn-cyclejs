204904f4b00d743b4a1233de16b6ab1c8d2decdf
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-09-25 11:28:52 +0530

    Cycle.js - interactive counter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
+import Rx from 'rx'
 import { run } from '@cycle/core'
-import { label, input, h1, hr, div, makeDOMDriver } from '@cycle/dom'
+import { button, p, label, div, makeDOMDriver } from '@cycle/dom'
 
 const
   // Logic (functional)
   main = sources => {
     const
-      inputEv$ = sources.DOM.select('.field').events('input'),
-      name$ = inputEv$.map(e => e.target.value).startWith('')
+      dec$ = sources.DOM.select('#decrement').events('click'),
+      inc$ = sources.DOM.select('#increment').events('click'),
+      decAct$ = dec$.map(() => -1),
+      incAct$ = inc$.map(() => 1),
+      number$ = Rx.Observable.of(10)
+        .merge(incAct$)
+        .merge(decAct$)
+        .scan((acc, val) => acc + val)
 
     return {
-      'DOM': name$.map(name =>
+      'DOM': number$.map(number =>
         div([
-          label('Name:'),
-          input('.field', { 'type': 'text' }),
-          hr(),
-          h1(`Hello ${name}!`)
+          button('#decrement', 'Decrement'),
+          button('#increment', 'Increment'),
+          p([
+            label(String(number))
+          ])
         ])
       )
     }
