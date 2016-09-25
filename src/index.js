f4cdb5294b0822367a99d44686980ef601f09ee2
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-09-25 12:47:39 +0530

    HTTP Driver

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
 import Rx from 'rx'
 import { run } from '@cycle/core'
-import { button, p, label, div, makeDOMDriver } from '@cycle/dom'
+import { button, h1, h4, a, div, makeDOMDriver } from '@cycle/dom'
+import { makeHTTPDriver } from '@cycle/http'
 
 const
   // Logic (functional)
   main = sources => {
     const
-      dec$ = sources.DOM.select('#decrement').events('click'),
-      inc$ = sources.DOM.select('#increment').events('click'),
-      decAct$ = dec$.map(() => -1),
-      incAct$ = inc$.map(() => 1),
-      number$ = Rx.Observable.of(10)
-        .merge(incAct$)
-        .merge(decAct$)
-        .scan((acc, val) => acc + val)
+      url = 'http://jsonplaceholder.typicode.com/users/1',
+      click$ = sources.DOM.select('.get-first').events('click'),
+      request$ = click$.map(() => ({
+        url,
+        'method': 'get'
+      })),
+
+      response$$ = sources.HTTP
+        .filter(r$ => r$.request.url === url),
+
+      resp$ = response$$.switch(),
+      firstUser$ = resp$.map(res => res.body).startWith(null)
 
     return {
-      'DOM': number$.map(number =>
+      'DOM': firstUser$.map(user =>
         div([
-          button('#decrement', 'Decrement'),
-          button('#increment', 'Increment'),
-          p([
-            label(String(number))
+          button('.get-first', 'Get first user'),
+          null === user ? null : div('.user-details', [
+            h1('.user-name', user.name),
+            h4('.user-email', user.email),
+            a('.user-website', { 'href': user.website }, user.website)
           ])
         ])
-      )
+      ),
+      'HTTP': request$
     }
   },
 
-  drivers = { 'DOM': makeDOMDriver('#app') }
+  drivers = {
+    'DOM': makeDOMDriver('#app'),
+    'HTTP': makeHTTPDriver()
+  }
 
 run(main, drivers)
