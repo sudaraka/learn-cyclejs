81671f6e56cc1d688fe3a9e856468c9d0563653a
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-09-25 21:58:09 +0530

    Model, View, Intent

--- src/index.js
+++ src/index.js
@@ -6,54 +6,61 @@ const
   // DOM Read: Detect slider change
   // Recalculate BMI
   // DOM Write: Display DMI
+
+  intent = domSource => ({
+    'changeWeight$': domSource.select('.weight').events('input')
+      .map(e => e.target.value),
+    'changeHeight$': domSource.select('.height').events('input')
+      .map(e => e.target.value)
+  }),
+
+  model = (changeWeight$, changeHeight$) => Rx.Observable.combineLatest(
+    changeWeight$.startWith(69),
+    changeHeight$.startWith(154),
+    (weight, height) => {
+      const
+        heightM = height / 100,
+        bmi = Math.round(weight / (heightM * heightM))
+
+      return {
+        bmi,
+        weight,
+        height
+      }
+    }
+  ),
+
+  view = state$ => state$.map(state =>
+    div([
+      div([
+        label(`Weight: ${state.weight}Kg`),
+        input('.weight', {
+          'type': 'number',
+          'min': 40,
+          'max': 150,
+          'value': state.weight
+        })
+      ]),
+      div([
+        label(`Height: ${state.height}cm`),
+        input('.height', {
+          'type': 'number',
+          'min': 140,
+          'max': 220,
+          'value': state.height
+        })
+      ]),
+      h2(`BMI is ${state.bmi}`)
+    ])
+  ),
+
   main = sources => {
     const
-      changeWeight$ = sources.DOM.select('.weight').events('input')
-        .map(e => e.target.value),
-      changeHeight$ = sources.DOM.select('.height').events('input')
-        .map(e => e.target.value),
+      { changeWeight$, changeHeight$ } = intent(sources.DOM),
+      state$ = model(changeWeight$, changeHeight$),
+      vtree$ = view(state$)
 
-      state$ = Rx.Observable.combineLatest(
-        changeWeight$.startWith(69),
-        changeHeight$.startWith(154),
-        (weight, height) => {
-          const
-            heightM = height / 100,
-            bmi = Math.round(weight / (heightM * heightM))
-
-          return {
-            bmi,
-            weight,
-            height
-          }
-        }
-      )
-
-    return {
-      'DOM': state$.map(state =>
-        div([
-          div([
-            label(`Weight: ${state.weight}Kg`),
-            input('.weight', {
-              'type': 'number',
-              'min': 40,
-              'max': 150,
-              'value': state.weight
-            })
-          ]),
-          div([
-            label(`Height: ${state.height}cm`),
-            input('.height', {
-              'type': 'number',
-              'min': 140,
-              'max': 220,
-              'value': state.height
-            })
-          ]),
-          h2(`BMI is ${state.bmi}`)
-        ])
-      )
-    }
+    return { 'DOM': vtree$ }
   },
 
   drivers = { 'DOM': makeDOMDriver('#app') }
