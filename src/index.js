47dc783f38677349cbe93498f0a71d3913bcb9b9
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2016-09-25 10:17:42 +0530

    Cycle.js - Hello World

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
-import Rx from 'rxjs'
 import { run } from '@cycle/core'
-import { h1, span, makeDOMDriver } from '@cycle/dom'
+import { label, input, h1, hr, div, makeDOMDriver } from '@cycle/dom'
 
 const
   // Logic (functional)
-  main = source => {
+  main = sources => {
     const
-      mouseover$ = source.DOM.select('span').events('mouseover')
+      inputEv$ = sources.DOM.select('.field').events('input'),
+      name$ = inputEv$.map(e => e.target.value).startWith('')
 
     return {
-      'DOM': mouseover$
-        .startWith(null)
-        .switchMap(() => Rx.Observable.timer(0, 1000)
-          .map(i => h1([ span([ `Seconds elapsed ${i}` ]) ]))
-        ),
-      'Log': Rx.Observable.timer(0, 2000).map(i => i * 2)
+      'DOM': name$.map(name =>
+        div([
+          label('Name:'),
+          input('.field', { 'type': 'text' }),
+          hr(),
+          h1(`Hello ${name}!`)
+        ])
+      )
     }
   },
 
+  drivers = { 'DOM': makeDOMDriver('#app') }
 
-// Effects (imperative)
-  consoleLogDriver = msg$ => msg$.subscribe(msg => console.log(msg)),
-
-  driversToUse = {
-    'DOM': makeDOMDriver('#app'),
-    'Log': consoleLogDriver
-  }
-
-run(main, driversToUse)
+run(main, drivers)
